87e4b25530fd56479bef87bed0ac810191e3922c
ccocchi <cocchi.c@gmail.com>
2013-06-04 19:04:43 +0200

    Add tests (to be rebase)

--- test/base_renderer_test.rb
+++ test/base_renderer_test.rb
@@ -15,6 +15,7 @@ class TestBaseRenderer < ActiveSupport::TestCase
     @context.assigns['data'] = @data
 
     @template = RablRails::CompiledTemplate.new
+    @template.source = {}
     @template.data = :@data
   end
 
@@ -29,7 +30,18 @@ class TestBaseRenderer < ActiveSupport::TestCase
 
   test "properly handle assigns with symbol keys" do
     @context.assigns[:foo] = 'bar'
-    @template.source = {}
     assert_nothing_raised { render_hash }
+  end
+
+  test "cache should be applied if no cache key is given" do
+    Rails.cache.should_not_receive(:fetch)
+    render_hash
+  end
+
+  test "cache should not be used if disabled in Rails configuration" do
+    ActionController::Base.stub(:perform_caching).and_return(false)
+    Rails.cache.should_not_receive(:fetch)
+    @template.cache_key = 'something'
+    render_hash
   end
 end
